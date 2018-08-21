35f6f6f81b05091dfb029e86e94dae1738a1db83
karthikhacker <karthik.hacker85@gmail.com>
2018-08-21 12:23:57 +0530

    Added scrool to top button

--- js/main.js
+++ js/main.js
@@ -18,3 +18,18 @@ $('#nav').onePageNav({
 });
 //wow js
 new WOW().init();
+//scroll to top
+window.onscroll = function(){
+    scrollFunction();
+}
+function scrollFunction(){
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    document.getElementById("btn").style.display = "block";
+  }else{
+    document.getElementById("btn").style.display = "none";
+  }
+}
+function topFunction(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
